816c6e1ee0d04a82900c05e04c48f82b0fea8951
rubenquadros <rquadros95@gmail.com>
2021-07-31 21:42:05 +0530

    initial commit

---
+++ app/src/main/java/com/ruben/epicworld/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ruben.epicworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ruben.epicworld.ui.theme.EpicWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EpicWorldTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EpicWorldTheme {
+        Greeting("Android")
+    }
+}
